51ab0dafa053a1126d5aecf2ceeb7349d7b7e7ca
Cole Winslow <colethomaswinslow7@gmail.com>
2021-03-13 10:53:30 -0700

    added api

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const submit = document.querySelector("#submit");
 var searchBar = document.querySelector("#searchBar")
 
 function getWeather(city){
-    const weatherUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const weatherUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     console.log(weatherUrl)
     fetch(weatherUrl)
     .then((data)=> data.json())
     .then((data => {
         console.log(data)
+        console.log(data.main.feels_like);
+        apiContainer.innerHTML="";
+        const cloudsEl =document.createElement("p");
+        cloudsEl.textContent= "weather:" + " " + (data.weather[0].description);
+        apiContainer.append(cloudsEl);
+
+        const tempEl = document.createElement("p");
+        tempEl.textContent= "temp:" + " " + (data.main.feels_like) + " " + "degrees";
+        apiContainer.append(tempEl);
+
+        
     }))
 }
 function handleSubmit(){
